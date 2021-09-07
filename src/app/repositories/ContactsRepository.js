e835a3a45240d53a2e2058b05e79645c10445de6
Gabriel Teodoro <oi@gabrielteodoro.com>
2021-09-07 13:05:57 -0300

    feat(contacts): added show and delete contacts

--- src/app/repositories/ContactsRepository.js
+++ src/app/repositories/ContactsRepository.js
@@ -1,10 +1,17 @@
 const { v4: uuid } = require('uuid');
 
-const contacts = [
+let contacts = [
   {
     id: uuid(),
     name: 'Gabriel',
     email: 'gabriel@mail.com',
+    phone: '123123123',
+    category_id: uuid(),
+  },
+  {
+    id: uuid(),
+    name: 'Mateus',
+    email: 'mateus@mail.com',
     phone: '123123123',
     category_id: uuid(),
   },
@@ -13,6 +20,19 @@ const contacts = [
 class ContactsRepository {
   findAll() {
     return new Promise((resolve) => resolve(contacts));
+  }
+
+  findById(id) {
+    return new Promise((resolve) => resolve(
+      contacts.find((contact) => contact.id === id),
+    ));
+  }
+
+  delete(id) {
+    return new Promise((resolve) => {
+      contacts = contacts.filter((contact) => contact.id !== id);
+      resolve();
+    });
   }
 }
 
