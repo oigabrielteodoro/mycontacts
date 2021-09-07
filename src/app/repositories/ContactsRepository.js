cdc4377f75940515b981bd40dc07eb1ba2dce2a5
Gabriel Teodoro <oi@gabrielteodoro.com>
2021-09-07 16:08:08 -0300

    feat(contacts): added db query for create, findAll, findById and findByEmail

--- src/app/repositories/ContactsRepository.js
+++ src/app/repositories/ContactsRepository.js
@@ -1,5 +1,7 @@
 const { v4: uuid } = require('uuid');
 
+const db = require('../../database');
+
 let contacts = [
   {
     id: uuid(),
@@ -18,20 +20,19 @@ let contacts = [
 ];
 
 class ContactsRepository {
-  findAll() {
-    return new Promise((resolve) => resolve(contacts));
+  async findAll() {
+    const rows = await db.query('SELECT * FROM contacts');
+    return rows;
   }
 
-  findById(id) {
-    return new Promise((resolve) => resolve(
-      contacts.find((contact) => contact.id === id),
-    ));
+  async findById(id) {
+    const [row] = await db.query('SELECT * FROM contacts WHERE id = $1', [id]);
+    return row;
   }
 
-  findByEmail(email) {
-    return new Promise((resolve) => resolve(
-      contacts.find((contact) => contact.email === email),
-    ));
+  async findByEmail(email) {
+    const [row] = await db.query('SELECT * FROM contacts WHERE email = $1', [email]);
+    return row;
   }
 
   delete(id) {
@@ -41,17 +42,16 @@ class ContactsRepository {
     });
   }
 
-  create({
-    name, email, phone, category_id,
+  async create({
+    name, email, phone,
   }) {
-    return new Promise((resolve) => {
-      const contact = {
-        id: uuid(), name, email, phone, category_id,
-      };
+    const [row] = await db.query(`
+      INSERT INTO contacts(name, email, phone)
+      VALUES($1, $2, $3)
+      RETURNING *
+    `, [name, email, phone]);
 
-      contacts = [...contacts, contact];
-      resolve(contact);
-    });
+    return row;
   }
 
   update(id, {
